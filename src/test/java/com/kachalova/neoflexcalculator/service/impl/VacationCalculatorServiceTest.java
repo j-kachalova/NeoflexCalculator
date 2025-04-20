e250c27016da6487ce315e6b5cf2ffa41d881f02
j-kachalova <62459071+j-kachalova@users.noreply.github.com>
2025-04-21 00:12:40 +0400

    change README

--- src/test/java/com/kachalova/neoflexcalculator/service/impl/VacationCalculatorServiceTest.java
+++ src/test/java/com/kachalova/neoflexcalculator/service/impl/VacationCalculatorServiceTest.java
@@ -18,21 +18,21 @@ public class VacationCalculatorServiceTest {
 
     @Test
     void testCalculateByVacationDays() {
-        VacationPayRequest request = VacationPayRequest.builder().averageSalary(BigDecimal.valueOf(90000)).vacationDays(14).build();
+        VacationPayRequest request = VacationPayRequest.builder().averageSalary(BigDecimal.valueOf(50000)).vacationDays(8).build();
 
         BigDecimal result = service.calculate(request);
-        assertEquals(BigDecimal.valueOf(43003.38), result);
+        assertEquals(BigDecimal.valueOf(13651.84), result);
     }
 
     @Test
     void testCalculateByDatesWithoutHolidays() {
         VacationPayRequest request = new VacationPayRequest();
-        request.setAverageSalary(BigDecimal.valueOf(90000));
-        request.setStartDate(LocalDate.of(2025, 6, 1));
-        request.setEndDate(LocalDate.of(2025, 6, 15));
+        request.setAverageSalary(BigDecimal.valueOf(50000));
+        request.setStartDate(LocalDate.of(2025, 4, 1));
+        request.setEndDate(LocalDate.of(2025, 4, 10));
 
         BigDecimal result = service.calculate(request);
         // Зависит от количества рабочих дней в интервале
-        assertEquals(BigDecimal.valueOf(27645.03), result);
+        assertEquals(BigDecimal.valueOf(13651.84), result);
     }
 }
